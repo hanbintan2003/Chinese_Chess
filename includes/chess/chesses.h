f64272acd9925103f75455a627a8174a9ca5d7cf
hanbintan2003 <thb182003@gmail.com>
2023-06-14 01:37:59 -0700

    logic clear

--- includes/chess/chesses.h
+++ includes/chess/chesses.h
@@ -12,98 +12,98 @@ private:
         Chess bc1( 28,60,0,0,config.get_texture("bc"));
         this->_chesses.push_back(bc1);
 
-        Chess bc2( 29,60,0,8,config.get_texture("bc"));
+        Chess bc2( 29,60,8,0,config.get_texture("bc"));
         this->_chesses.push_back(bc2);
 
-        Chess bm1(30,60,0,1,config.get_texture("bm"));
+        Chess bm1(30,60,1,0,config.get_texture("bm"));
         this->_chesses.push_back(bm1);
 
-        Chess bm2(31,60,0,7,config.get_texture("bm"));
+        Chess bm2(31,60,7,0,config.get_texture("bm"));
         this->_chesses.push_back(bm2);
 
-        Chess bx1(32, 60,0,2,config.get_texture("bx"));
+        Chess bx1(32, 60,2,0,config.get_texture("bx"));
         this->_chesses.push_back(bx1);
 
-        Chess bx2(33,60,0,6,config.get_texture("bx"));
+        Chess bx2(33,60,6,0,config.get_texture("bx"));
         this->_chesses.push_back(bx2);
 
-        Chess bs1(34, 60,0,3,config.get_texture("bs"));
+        Chess bs1(34, 60,3,0,config.get_texture("bs"));
         this->_chesses.push_back(bs1);
 
-        Chess bs2(35, 60,0,5,config.get_texture("bs"));
+        Chess bs2(35, 60,5,0,config.get_texture("bs"));
         this->_chesses.push_back(bs2);
 
-        Chess bj(36, 60,0,4,config.get_texture("bj"));
+        Chess bj(36, 60,4,40,config.get_texture("bj"));
         this->_chesses.push_back(bj);
 
-        Chess bp1(26, 60,2,1,config.get_texture("bp"));
+        Chess bp1(26, 60,1,2,config.get_texture("bp"));
         this->_chesses.push_back(bp1);
 
-        Chess bp2(27, 60,2,7,config.get_texture("bp"));
+        Chess bp2(27, 60,7,2,config.get_texture("bp"));
         this->_chesses.push_back(bp2);
 
-        Chess bz1(21, 60,3,0,config.get_texture("bz"));
+        Chess bz1(21, 60,0,3,config.get_texture("bz"));
         this->_chesses.push_back(bz1);
 
-        Chess bz2(22, 60,3,2,config.get_texture("bz"));
+        Chess bz2(22, 60,2,3,config.get_texture("bz"));
         this->_chesses.push_back(bz2);
 
-        Chess bz3(23, 60,3,4,config.get_texture("bz"));
+        Chess bz3(23, 60,4,3,config.get_texture("bz"));
         this->_chesses.push_back(bz3);
 
-        Chess bz4(24, 60,3,6,config.get_texture("bz"));
+        Chess bz4(24, 60,6,3,config.get_texture("bz"));
         this->_chesses.push_back(bz4);
 
-        Chess bz5(25, 60,3,8,config.get_texture("bz"));
+        Chess bz5(25, 60,8,3,config.get_texture("bz"));
         this->_chesses.push_back(bz5);
 
 
-        Chess rc1(8, 60,9,0,config.get_texture("rc"));
+        Chess rc1(8, 60,0,9,config.get_texture("rc"));
         this->_chesses.push_back(rc1);
 
-        Chess rc2(9, 60,9,8,config.get_texture("rc"));
+        Chess rc2(9, 60,8,9,config.get_texture("rc"));
         this->_chesses.push_back(rc2);
 
-        Chess rm1(10, 60,9,1,config.get_texture("rm"));
+        Chess rm1(10, 60,1,9,config.get_texture("rm"));
         this->_chesses.push_back(rm1);
 
-        Chess rm2(11, 60,9,7,config.get_texture("rm"));
+        Chess rm2(11, 60,7,9,config.get_texture("rm"));
         this->_chesses.push_back(rm2);
 
-        Chess rx1(12, 60,9,2,config.get_texture("rx"));
+        Chess rx1(12, 60,2,9,config.get_texture("rx"));
         this->_chesses.push_back(rx1);
 
-        Chess rx2(13, 60,9,6,config.get_texture("rx"));
+        Chess rx2(13, 60,6,9,config.get_texture("rx"));
         this->_chesses.push_back(rx2);
 
-        Chess rs1(14,60,9,3,config.get_texture("rs"));
+        Chess rs1(14,60,3,9,config.get_texture("rs"));
         this->_chesses.push_back(rs1);
 
-        Chess rs2(15, 60,9,5,config.get_texture("rs"));
+        Chess rs2(15, 60,5,9,config.get_texture("rs"));
         this->_chesses.push_back(rs2);
 
-        Chess rj(16,60,9,4,config.get_texture("rj"));
+        Chess rj(16,60,4,9,config.get_texture("rj"));
         this->_chesses.push_back(rj);
 
-        Chess rp1(6, 60,7,1,config.get_texture("rp"));
+        Chess rp1(6, 60,1,7,config.get_texture("rp"));
         this->_chesses.push_back(rp1);
 
         Chess rp2( 7,60,7,7,config.get_texture("rp"));
         this->_chesses.push_back(rp2);
 
-        Chess rb1(1,60,6,0,config.get_texture("rb"));
+        Chess rb1(1,60,0,6,config.get_texture("rb"));
         this->_chesses.push_back(rb1);
 
-        Chess rb2(2,60,6,2,config.get_texture("rb"));
+        Chess rb2(2,60,2,6,config.get_texture("rb"));
         this->_chesses.push_back(rb2);
 
-        Chess rb3( 3,60,6,4,config.get_texture("rb"));
+        Chess rb3( 3,60,4,6,config.get_texture("rb"));
         this->_chesses.push_back(rb3);
 
         Chess rb4( 4,60,6,6,config.get_texture("rb"));
         this->_chesses.push_back(rb4);
 
-        Chess rb5( 5,60,6,8,config.get_texture("rb"));
+        Chess rb5( 5,60,8,6,config.get_texture("rb"));
         this->_chesses.push_back(rb5);
 
         //forloop for grid
@@ -131,6 +131,7 @@ public:
     ~Chesses()= default;
 
     int update_chesses(sf::RenderWindow &window, sf::Event& event, int& play, int& name){
+        bool work = false;
         for(auto & _chesse : this->_chesses){
             if((_chesse.isMouseOver(window) && _chesse.getName() >0 && _chesse.getName() < 17 && play == 0 && name == -1)
             ||(_chesse.isMouseOver(window) && _chesse.getName() >20 && _chesse.getName() < 37 && play == 1 && name == -1))
@@ -170,33 +171,39 @@ public:
             if((clicked && (_chesse.getName()<1 || _chesse.getName()>16) && _chesse.getName()!=name && play == 0 && name >=1 &&name <6)||
             (clicked && _chesse.getName()<21) && _chesse.getName()!=name && play == 1&& name >=11 &&name <16)
             {
-                cout<<"stopby";
+                //cout<<"stopby";
                 int x = _chesse.getX();
                 int y = _chesse.getY();
 
 
                 for (auto & _chesse : this->_chesses) {
                     if(_chesse.getName() == name){
-                        cout<<"work";
+                        //cout<<"work";
                         if (play == 0){
                             cout<<_chesse.getY();
 
                             if (_chesse.getY() > 4){
-                                cout<<"work";
-                                if (x==_chesse.getX() && y==_chesse.getY()+1){
-                                    _chesse.move(_chesse.getX(),_chesse.getY()+1);
+                                //cout<<"work";
+                                if (x==_chesse.getX() && y==_chesse.getY()-1){
+                                    //cout<<"work";
+                                    _chesse.move(_chesse.getX(),_chesse.getY()-1);
                                 }
                             }
                         }
                     }
                 }
-                _chesse.move(_chesse.getX(),_chesse.getY()-1);
+                _chesse.move(_chesse.getX(),_chesse.getY()+1);
 
 
-                name = -1;
-                if (play == 1)play = 0;
-                else play = 1;
-                return -1;
+                if (work){
+                    name = -1;
+                    if (play == 1)play = 0;
+                    else play = 1;
+                    return -1;
+                }else{
+                    cout<<"no work";
+                    return name;
+                }
             }
 
 
--- includes/config/system.h
+++ includes/config/system.h
@@ -57,17 +57,16 @@ const sf::Vector2f HEADER_SIZE = sf::Vector2f(300, 50);
 const sf::Vector2f HEADER_POS = sf::Vector2f(430, 200);
 
 // chesses
-const sf::Vector2f grid[10][9] = {
-        { sf::Vector2f(0, 0), sf::Vector2f(152, 0), sf::Vector2f(310, 0), sf::Vector2f(460, 0), sf::Vector2f(612, 0), sf::Vector2f(765, 0), sf::Vector2f(920, 0), sf::Vector2f(1070, 0), sf::Vector2f(1212, 0) },
-        { sf::Vector2f(0, 140), sf::Vector2f(152, 140), sf::Vector2f(310, 140), sf::Vector2f(460, 140), sf::Vector2f(612, 140), sf::Vector2f(765, 140), sf::Vector2f(920, 140), sf::Vector2f(1070, 140), sf::Vector2f(1212, 140) },
-        { sf::Vector2f(0, 290), sf::Vector2f(152, 290), sf::Vector2f(310, 290), sf::Vector2f(460, 290), sf::Vector2f(612, 290), sf::Vector2f(765, 290), sf::Vector2f(920, 290), sf::Vector2f(1070, 290), sf::Vector2f(1212, 290) },
-        { sf::Vector2f(0, 440), sf::Vector2f(152, 440), sf::Vector2f(310, 440), sf::Vector2f(460, 440), sf::Vector2f(612, 440), sf::Vector2f(765, 440), sf::Vector2f(920, 440), sf::Vector2f(1070, 440), sf::Vector2f(1212, 440) },
-        { sf::Vector2f(0, 590), sf::Vector2f(152, 590), sf::Vector2f(310, 590), sf::Vector2f(460, 590), sf::Vector2f(612, 590), sf::Vector2f(765, 590), sf::Vector2f(920, 590), sf::Vector2f(1070, 590), sf::Vector2f(1212, 590) },
-        { sf::Vector2f(0, 740), sf::Vector2f(152, 740), sf::Vector2f(310, 740), sf::Vector2f(460, 740), sf::Vector2f(612, 740), sf::Vector2f(765, 740), sf::Vector2f(920, 740), sf::Vector2f(1070, 740), sf::Vector2f(1212, 740) },
-        { sf::Vector2f(0, 890), sf::Vector2f(152, 890), sf::Vector2f(310, 890), sf::Vector2f(460, 890), sf::Vector2f(612, 890), sf::Vector2f(765, 890), sf::Vector2f(920, 890), sf::Vector2f(1070, 890), sf::Vector2f(1212, 890) },
-        { sf::Vector2f(0, 1040), sf::Vector2f(152, 1040), sf::Vector2f(310, 1040), sf::Vector2f(460, 1040), sf::Vector2f(612, 1040), sf::Vector2f(765, 1040), sf::Vector2f(920, 1040), sf::Vector2f(1070, 1040), sf::Vector2f(1212, 1040) },
-        { sf::Vector2f(0, 1190), sf::Vector2f(152, 1190), sf::Vector2f(310, 1190), sf::Vector2f(460, 1190), sf::Vector2f(612, 1190), sf::Vector2f(765, 1190), sf::Vector2f(920, 1190), sf::Vector2f(1070, 1190), sf::Vector2f(1212, 1190) },
-        { sf::Vector2f(0, 1328), sf::Vector2f(152, 1328), sf::Vector2f(310, 1328), sf::Vector2f(460, 1328), sf::Vector2f(612, 1328), sf::Vector2f(765, 1328), sf::Vector2f(920, 1328), sf::Vector2f(1070, 1328), sf::Vector2f(1212, 1328) }
+const sf::Vector2f grid[9][10] = {
+        { sf::Vector2f(0, 0), sf::Vector2f(0, 140), sf::Vector2f(0, 290), sf::Vector2f(0, 440), sf::Vector2f(0, 590), sf::Vector2f(0, 740), sf::Vector2f(0, 890), sf::Vector2f(0, 1040), sf::Vector2f(0, 1190), sf::Vector2f(0, 1328) },
+        { sf::Vector2f(152, 0), sf::Vector2f(152, 140), sf::Vector2f(152, 290), sf::Vector2f(152, 440), sf::Vector2f(152, 590), sf::Vector2f(152, 740), sf::Vector2f(152, 890), sf::Vector2f(152, 1040), sf::Vector2f(152, 1190), sf::Vector2f(152, 1328) },
+        { sf::Vector2f(310, 0), sf::Vector2f(310, 140), sf::Vector2f(310, 290), sf::Vector2f(310, 440), sf::Vector2f(310, 590), sf::Vector2f(310, 740), sf::Vector2f(310, 890), sf::Vector2f(310, 1040), sf::Vector2f(310, 1190), sf::Vector2f(310, 1328) },
+        { sf::Vector2f(460, 0), sf::Vector2f(460, 140), sf::Vector2f(460, 290), sf::Vector2f(460, 440), sf::Vector2f(460, 590), sf::Vector2f(460, 740), sf::Vector2f(460, 890), sf::Vector2f(460, 1040), sf::Vector2f(460, 1190), sf::Vector2f(460, 1328) },
+        { sf::Vector2f(612, 0), sf::Vector2f(612, 140), sf::Vector2f(612, 290), sf::Vector2f(612, 440), sf::Vector2f(612, 590), sf::Vector2f(612, 740), sf::Vector2f(612, 890), sf::Vector2f(612, 1040), sf::Vector2f(612, 1190), sf::Vector2f(612, 1328) },
+        { sf::Vector2f(765, 0), sf::Vector2f(765, 140), sf::Vector2f(765, 290), sf::Vector2f(765, 440), sf::Vector2f(765, 590), sf::Vector2f(765, 740), sf::Vector2f(765, 890), sf::Vector2f(765, 1040), sf::Vector2f(765, 1190), sf::Vector2f(765, 1328) },
+        { sf::Vector2f(920, 0), sf::Vector2f(920, 140), sf::Vector2f(920, 290), sf::Vector2f(920, 440), sf::Vector2f(920, 590), sf::Vector2f(920, 740), sf::Vector2f(920, 890), sf::Vector2f(920, 1040), sf::Vector2f(920, 1190), sf::Vector2f(920, 1328) },
+        { sf::Vector2f(1070, 0), sf::Vector2f(1070, 140), sf::Vector2f(1070, 290), sf::Vector2f(1070, 440), sf::Vector2f(1070, 590), sf::Vector2f(1070, 740), sf::Vector2f(1070, 890), sf::Vector2f(1070, 1040), sf::Vector2f(1070, 1190), sf::Vector2f(1070, 1328) },
+        { sf::Vector2f(1212, 0), sf::Vector2f(1212, 140), sf::Vector2f(1212, 290), sf::Vector2f(1212, 440), sf::Vector2f(1212, 590), sf::Vector2f(1212, 740), sf::Vector2f(1212, 890), sf::Vector2f(1212, 1040), sf::Vector2f(1212, 1190), sf::Vector2f(1212, 1328) }
 };
 
 
